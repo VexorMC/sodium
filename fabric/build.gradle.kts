1efaf8eb577f5a742c2b8a8d2d5f060ed77d2ab1
Luna0x01 <me@lunasa.dev>
2025-08-09 19:05:57 +0530

    Remove conflicting LWJGL 3 dependency entries from Fabric module buildscript

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -29,26 +29,7 @@ dependencies {
 
     shadow("org.joml:joml:1.10.8")
     shadow("it.unimi.dsi:fastutil:8.5.15")
-
-    shadow("org.lwjgl", "lwjgl", version = "3.3.2", classifier = "natives-linux")
-    shadow("org.lwjgl", "lwjgl", version = "3.3.2", classifier = "natives-windows")
-
-    shadow(platform("org.lwjgl:lwjgl-bom:3.3.5"))
-
-    shadow("org.lwjgl:lwjgl")
-    shadow("org.lwjgl:lwjgl-glfw")
-    shadow("org.lwjgl:lwjgl-openal")
-
     shadow("org.jetbrains:annotations:26.0.2")
-    shadow(project(":lwjgl3", configuration = "default"))
-
-    arrayOf("linux", "windows", "macos", "windows-arm64", "macos-arm64").forEach { platform ->
-        shadow("org.lwjgl:lwjgl::natives-$platform")
-        shadow("org.lwjgl:lwjgl-glfw::natives-$platform")
-        shadow("org.lwjgl:lwjgl-openal::natives-$platform")
-        shadow("org.lwjgl:lwjgl-opengl::natives-$platform")
-    }
-
 }
 
 sourceSets.apply {
