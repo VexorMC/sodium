123f1dabb0c6f035bd183d5f099b99a7e1b6fb4e
Luna0x01 <me@lunasa.dev>
2025-12-12 20:06:51 +0530

    Remove unused properties from BuildConfig.kt

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,10 +3,6 @@
 object BuildConfig {
     val MINECRAFT_VERSION: String = "1.8.9"
     val FABRIC_LOADER_VERSION: String = "0.16.9"
-    val FABRIC_API_VERSION: String = "1.9.4+1.8.9"
-
-    // This value can be set to null to disable Parchment.
-    val PARCHMENT_VERSION: String? = "2024.12.07"
 
     // https://semver.org/
     var MOD_VERSION: String = "0.8.7"
