b28163d97655722b5c12825acc7fe39986f639a3
LunasaVT <me@lunasa.dev>
2026-04-07 01:25:19 +0530

    Bump to 0.8.10

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -5,7 +5,7 @@ object BuildConfig {
     val FABRIC_LOADER_VERSION: String = "0.16.9"
 
     // https://semver.org/
-    var MOD_VERSION: String = "0.8.9"
+    var MOD_VERSION: String = "0.8.10"
 
     fun createVersionString(project: Project): String {
         val builder = StringBuilder()
